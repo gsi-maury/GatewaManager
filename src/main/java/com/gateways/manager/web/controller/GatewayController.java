dc1b5430ebc06443bdb5a6b1dadaf2ccfeaba127
gsi-maury <gsi-maury@github.com>
2019-05-22 12:20:22 -0500

    feat: expose addGateway from GatewayController

--- src/main/java/com/gateways/manager/web/controller/GatewayController.java
+++ src/main/java/com/gateways/manager/web/controller/GatewayController.java
@@ -1,12 +1,13 @@
 package com.gateways.manager.web.controller;
 
-import com.gateways.manager.domain.entity.Gateway;
 import com.gateways.manager.domain.factory.GatewayFactory;
 import com.gateways.manager.domain.service.GatewayService;
 import com.gateways.manager.web.assembler.GatewayResourceAssembler;
-import com.gateways.manager.web.assembler.resource.GatewayResource;
 import com.gateways.manager.web.dto.GatewayDTO;
+import com.gateways.manager.web.dto.validator.GatewayDTOValidator;
+import com.gateways.manager.web.util.HeaderMsgUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,4 +29,19 @@ public class GatewayController {
         this.gatewayAssembler = gatewayAssembler;
     }
 
+    @PostMapping
+    public ResponseEntity saveGateway(@RequestBody GatewayDTO gatewayDTO) {
+        GatewayDTOValidator validator = new GatewayDTOValidator(gatewayDTO);
+
+        if (!validator.isValid())
+            return HeaderMsgUtil.buildMsg(ResponseEntity.unprocessableEntity(),
+                    validator.getErrors().toArray(new String[0]));
+
+        if (gatewayService.getGateway(gatewayDTO.getSerialNumber()).isPresent())
+            return HeaderMsgUtil.buildMsg(
+                    ResponseEntity.status(HttpStatus.CONFLICT), "This Gateway already exist");
+
+        return ResponseEntity.ok(gatewayAssembler.toResource(
+                gatewayService.saveGateway(gatewayFactory.from(gatewayDTO))));
+    }
 }
